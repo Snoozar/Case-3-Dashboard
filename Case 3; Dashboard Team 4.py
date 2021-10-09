6c4c82667240be42ecdb6edf69b4f90a4192af5a
Snoozar <vince.kemme@gmail.com>
2021-10-09 14:28:29 +0200

    Add files via upload

---
+++ Case 3; Dashboard Team 4.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+import os
+import requests
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
+
+
+# In[3]:
+
+
+get_ipython().system('pip install streamlit')
+
+
+# In[5]:
+
+
+st.title("Test")
+
