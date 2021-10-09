dfcfbf3e279783ca9ed7489331475a8664912ae2
Snoozar <vince.kemme@gmail.com>
2021-10-09 16:08:10 +0200

    Add files via upload

---
+++ Case3DashboardTeam4.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+import os
+import requests
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
+
+
+# In[5]:
+
+
+st.title("Test")
+
